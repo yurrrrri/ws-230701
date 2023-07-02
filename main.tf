d00907ea944cc609975e432edaf6e7658f9a04f9
yurison <yu_ri_son@naver.com>
2023-07-03 00:29:59 +0900

    variables.tf 도입

--- main.tf
+++ main.tf
@@ -1,20 +1,38 @@
 terraform {
   cloud {
-    organization = "og-1"
+    organization = "yuri-og-1"
 
     workspaces {
-      name = "ws-1"
+      name = "yuri-ws-1"
     }
   }
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-northeast-2"
+  region = var.region
+}
+
+resource "aws_vpc" "vpc_1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.prefix}-vpc-1"
+  }
+}
+
+resource "aws_subnet" "sub_1" {
+  vpc_id = aws_vpc.vpc_1.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.prefix}-sub-1"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default = "dev"
+}
+
+variable "region" {
+  description = "region"
+  default = "ap-northeast-2"
+}
